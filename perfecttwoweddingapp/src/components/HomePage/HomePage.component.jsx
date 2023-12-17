2930dcd9e369941653414a9fa01348d2beb9f69c
Clark <a1clark1a@gmail.com>
2023-12-16 21:23:07 -0800

    Initial CRA

---
+++ perfecttwoweddingapp/src/components/HomePage/HomePage.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <section>
+      <div>
+        <h2>Clock</h2>
+      </div>
+      <div>
+        <h1>Sara & Clark</h1>
+      </div>
+      <div>
+        <h2>Date</h2>
+      </div>
+      <div>
+        <button>RSVP</button>
+      </div>
+    </section>
+  );
+};
+
+export default HomePage;
